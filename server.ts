fcf87e3af968eef29e55251bc978cce92c5a432d
Alisha <mahmoodalisha35@gmail.com>
2024-09-18 18:44:06 +0530

    changed cors

--- server.ts
+++ server.ts
@@ -11,7 +11,9 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
+
+app.use('/files', express.static(path.join(__dirname, 'uploads')));
 
 // Add Cache-Control middleware
 app.use((req, res, next) => {
